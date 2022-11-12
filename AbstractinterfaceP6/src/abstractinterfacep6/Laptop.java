2917a93ce02d03a1fcb3eded50f3ed733b4b23cb
Ario Febri <84925625+awriyou@users.noreply.github.com>
2022-11-12 23:05:00 +0700

    Add files via upload

---
+++ AbstractinterfaceP6/src/abstractinterfacep6/Laptop.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package abstractinterfacep6;
+
+/**
+ *
+ * @author febri
+ */
+
+public interface Laptop {
+    int MAX_VOL=100;
+    int MIN_VOL=0;
+
+    void powerOn();
+    void powerOff();
+    void volumeUp();
+    void volumeDown();
+}
+    
+
---
+++ AbstractinterfaceP6/src/abstractinterfacep6/LaptopUser.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package abstractinterfacep6;
+
+/**
+ *
+ * @author febri
+ */
+public class LaptopUser {
+    private final Laptop laptop;
+
+    public LaptopUser(Laptop laptop){
+        this.laptop=laptop;
+    }
+
+    void turnOnLaptop(){
+        this.laptop.powerOn();
+    }
+
+    void turnOffLaptop(){
+        this.laptop.powerOff();
+    }
+
+    void makeLaptopLouder(){
+        this.laptop.volumeUp();
+    }
+
+    void makeLaptopSilence(){
+        this.laptop.volumeDown();
+    }
+}
